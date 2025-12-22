4fc123f6b8468801f70953e0ffaaddc64e95dc2e
EqScript <sergio.belenkoff@gmail.com>
2025-12-22 21:24:14 +0700

    hyprland, yandex-disk updated

--- sway/wallpapers/random-wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# random-wallpaper.sh
-
-WALLPAPER_DIR="$HOME/dotfiles/sway/wallpapers"
-RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f -iname '*.jpg' | sort -R | head -n 1)
-
-[ -n "$RANDOM_WALLPAPER" ] && swaymsg output "*" bg "$RANDOM_WALLPAPER" fill
