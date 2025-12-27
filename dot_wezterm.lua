7723f86ca719a835e274c46e4e9355af3038053f
EqScript <sergio.belenkoff@gmail.com>
2025-12-27 16:25:52 +0700

    fastfetch added, aliases, wezterm theme

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,14 +5,18 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
+local shell = "bash"
 
 -- For example, changing the initial geometry for new windows:
 -- config.initial_cols = 120
 -- config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 16
-config.color_scheme = 'Breath Silverfox (Gogh)'
+config.font_size = 14
+config.color_scheme = 'Dissonance (Gogh)'
+
+-- Welcome screen
+config.default_prog = { "bash", "-c", "fastfetch --logo none; bash -l" }
 
 -- Finally, return the configuration to wezterm:
 return config
