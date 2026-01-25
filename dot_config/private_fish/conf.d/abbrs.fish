90579325b9b9fc3a7a30350da1ebe4f91236a5f2
EqScript <rockzone@mail.ru>
2026-01-25 15:52:34 +0700

    starfish prompt updated

--- dot_config/private_fish/conf.d/abbrs.fish
+++ dot_config/private_fish/conf.d/abbrs.fish
@@ -6,3 +6,4 @@ abbr -a -- cl clear
 abbr -a -- abdump 'abbr > $HOME/.config/fish/conf.d/abbrs.fish'
 abbr -a -- sus 'systemctl suspend'
 abbr -a -- chapply 'chezmoi apply'
+abbr -a -- chst 'chezmoi status'
